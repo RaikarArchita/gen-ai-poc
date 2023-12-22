af3cbd53bf5d4130d22ba2f392dbaff6c9e35803
Raikar, Archita <archita.raikar@accenture.com>
2023-12-22 18:48:17 +0530

    added changes for background

--- src/components/MinimizableWebChat/MinimizableWebChat.jsx
+++ src/components/MinimizableWebChat/MinimizableWebChat.jsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
-import React, { useCallback, useMemo, useState } from 'react';
-import {  createStyleSet } from 'botframework-webchat';
+import React, { Fragment, useCallback, useMemo, useState } from 'react';
+import { createStyleSet } from 'botframework-webchat';
 import './fabric-icons-inline.css';
 import './MinimizableWebChat.css';
 import WebChat from '../WebChat/WebChat';
@@ -18,13 +18,13 @@ const MinimizableWebChat = () => {
   const [loaded, setLoaded] = useState(false);
   const [minimized, setMinimized] = useState(true);
   const [newMessage, setNewMessage] = useState(false);
-  const [side, setSide] = useState('right');
+  const [side, setSide] = useState('center');
   const [token, setToken] = useState();
 
   const handleFetchToken = useCallback(async () => {
     if (!token) {
-      const res = await fetch('https://382265f2b50d45859dbfc7ca298856.a8.environment.api.powerplatform.com/powervirtualagents/botsbyschema/new_gira/directline/token?api-version=2022-03-01-preview', 
-      { method: 'GET' });
+      const res = await fetch('https://382265f2b50d45859dbfc7ca298856.a8.environment.api.powerplatform.com/powervirtualagents/botsbyschema/new_gira/directline/token?api-version=2022-03-01-preview',
+        { method: 'GET' });
       const { token } = await res.json();
       setToken(token);
     }
@@ -39,40 +39,50 @@ const MinimizableWebChat = () => {
   const handleMinimizeButtonClick = useCallback(() => {
     setMinimized(true);
     setNewMessage(false);
+    setLoaded(false);
   }, [setMinimized, setNewMessage]);
 
-  const handleSwitchButtonClick = useCallback(() => {
-    setSide(side === 'left' ? 'right' : 'left');
-  }, [setSide, side]);
+  // const handleSwitchButtonClick = useCallback(() => {
+  //   setSide(side === 'left' ? 'right' : 'left');
+  // }, [setSide, side]);
 
   return (
-    <div className="minimizable-web-chat">
-      {minimized && (
-        <button className="maximize" onClick={handleMaximizeButtonClick}>
-          <span className={token ? 'ms-Icon ms-Icon--MessageFill' : 'ms-Icon ms-Icon--Message'} />
-          {newMessage && <span className="ms-Icon ms-Icon--CircleShapeSolid red-dot" />}
-        </button>
-      )}
-      {loaded && (
-        <div className={classNames(side === 'left' ? 'chat-box left' : 'chat-box right', minimized ? 'hide' : '')}>
-          <header>
-            <div className="filler" />
-            <button className="switch" onClick={handleSwitchButtonClick}>
-              <span className="ms-Icon ms-Icon--Switch" />
-            </button>
-            <button className="minimize" onClick={handleMinimizeButtonClick}>
-              <span className="ms-Icon ms-Icon--ChromeMinimize" />
-            </button>
-          </header>
-          <WebChat
-            className="react-web-chat"
-            onFetchToken={handleFetchToken}
-            styleSet={styleSet}
-            token={token}
-          />
-        </div>
-      )}
-    </div>
+    <Fragment>
+      {loaded && (<div style={{
+        backgroundColor: 'rgba(0,0,0,0.4)',
+        width: '100VW',
+        height: '100%',
+        zIndex: '30',
+        position: 'fixed',
+        top: '0',
+      }} />)}
+      <div className="minimizable-web-chat">
+        {minimized && (
+          <button className="maximize" onClick={handleMaximizeButtonClick}>
+            <span className={token ? 'ms-Icon ms-Icon--MessageFill' : 'ms-Icon ms-Icon--Message'} />
+            {newMessage && <span className="ms-Icon ms-Icon--CircleShapeSolid red-dot" />}
+          </button>
+        )}
+        {loaded && (
+          <div className={classNames(side === 'left' ? 'chat-box left' : side === 'right' ?
+            'chat-box right' : 'chat-box center',
+            minimized ? 'hide' : '')}>
+            <header>
+              <div className="filler" />
+              <button className="minimize" onClick={handleMinimizeButtonClick}>
+                <span className="ms-Icon ms-Icon--ChromeMinimize" />
+              </button>
+            </header>
+            <WebChat
+              className="react-web-chat"
+              onFetchToken={handleFetchToken}
+              styleSet={styleSet}
+              token={token}
+            />
+          </div>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
